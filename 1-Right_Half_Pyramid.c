3ada5923c569994358154057f474803872c33993
@curious_coder_aman <113257634+Amanyadav0007@users.noreply.github.com>
2023-12-25 02:02:57 +0530

    Add files via upload

---
+++ 1-Right_Half_Pyramid.c
@@ -0,0 +1,23 @@
+// Output : Right Half Pyramid
+// *
+// * *
+// * * *
+// * * * *
+
+#include<stdio.h>
+
+int main()
+{
+    int row;
+    printf("Enter the value of row : ");
+    scanf("%d", &row);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
